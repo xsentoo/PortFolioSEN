b5e1caa2c31f947d6b8b6e9bf90eae128064c423
Alexandros Georgiadis <kokoras19888@gmail.com>
2023-10-31 23:22:11 +0100

    fixed the small screen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,26 +5,24 @@ import { Link } from "react-scroll";
 
 
 const NavBar = () => {
-
-
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
   const links = [
     {
       id: 1,
-      link: 'home'
+      link: 'home',
     },
     {
       id: 2,
-      link: 'about'
+      link: 'about',
     },
     {
       id: 3,
-      link: 'experience'
+      link: 'experience',
     },
     {
       id: 4,
-      link: 'contact'
+      link: 'Contact',
     },
   ];
 
@@ -59,8 +57,15 @@ const NavBar = () => {
           <ul className='flex flex-col justify-center items-center absolute top-0 left-0
           w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
             {links.map(({ id, link }) => (
-              <li  key={id} className='px-4 cursor-pointer capitalize py-6 text-4xl'>
-                <Link to={link} smooth duration={500}>{link}</Link>
+              <li
+                  key={id}
+                  className='px-4 cursor-pointer capitalize py-6 text-4xl'>
+                <Link
+                    onClick={() => setNav(!nav)}
+                    to={link} smooth duration={500}
+                    >
+                      {link}
+                  </Link>
               </li>
             ))}
           </ul>
