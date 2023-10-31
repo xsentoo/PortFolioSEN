6a571c8bf232ade71f187ec844021597cfb2cf14
Alexandros Georgiadis <kokoras19888@gmail.com>
2023-10-31 23:59:00 +0100

    smth

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -56,10 +56,10 @@ const Experience = () => {
       >
       <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
         <div>
-            <p className='text-4xl font-bold border-b-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
               Experience
             </p>
-            <p className='py-6'>THese are the technologies i've worked with</p>
+            <p className='py-6'>These are the technologies i've worked with</p>
         </div>
 
         <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
