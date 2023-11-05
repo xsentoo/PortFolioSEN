d618b3fb0c056faba36ac862a5b519dbe259774e
Alexandros Georgiadis <kokoras19888@gmail.com>
2023-11-06 00:00:16 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 
+
 function App() {
   return (
     <div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import HeroImage from "../assets/heroImage.png";
+import HeroImage from "../assets/hero.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
+
 
 const Home = () => {
   return (
@@ -10,11 +11,9 @@ const Home = () => {
 
           <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
               <div className='flex flex-col justify-center h-full'>
-                  <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+                  <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Front End Developer</h2>
                   <p className='text-gray-500 py-4 max-w-md'>
-                    I have 8 year of experience and a very big dick.
-                    Do you want to see it? I like to fuck and play video games.
-                    How are you doing today little shit?
+                    Front End developer with knowlegde in Back End and UI/UX. I am looking  for the next coding experience
                   </p>
 
                   <div>
@@ -22,7 +21,7 @@ const Home = () => {
                       to="contact"
                       smooth
                       duration={500}
-                      className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 "
+                      className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer "
                       >
                         Want to work with me?
                         <span className='group-hover:rotate-90 duration-300'>
