3517aada53104088e56b9002ae383f6587074c27
Alexandros Georgiadis <kokoras19888@gmail.com>
2023-11-12 12:03:54 +0100

    new changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,18 +9,29 @@ const about = () => {
             </div>
 
 
-              <p className='text-xl mt-20'> My name is Alexandros Georgiadis. I am Greek and the last 5 years I living
+              <p className='text-xl mt-20'> My name is Alexandros Georgiadis. I am Greek and the last 5 years I am living
               and working in Bordeaux. I am a cabin crew supervisor and recently I finished a full-stack intence Bootcamp
               in web Develpment
-              Now I am looking forward to work as a developer  </p>
+              Now I am looking forward to start my career as web developer  </p>
 
 
               <br />
 
-              <p className='text-xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates,
-              placeat aperiam? Necessitatibus itaque
-              ipsa atque, quis placeat unde voluptatibus debitis nam distinctio
-              sapiente, a corporis, rem sed modi non libero!</p>
+              <p className='text-xl'> During the bootcamp we set up several Projects in teams of four.
+              The stacks we used are Ruby, Ruby in Rails, HTML5, SASS, SQL, Javascript and Github.The projects were
+              initialy designed on Figma and were deployd in Heroku.  </p>
+
+              <br />
+
+              <p className='text-xl'> After the bootcamp I kept studing new techs by my own.
+              So far I am studing React with Tailwind. I have additional knowledge in Wordpress by
+              using ready themes or even editing custom themes.</p>
+
+              <br />
+
+              <p className='text-xl'> My experiece in previous jobs tought me how to work
+              in a team and proactive under preasure. I believe that my passion for coding and new technologies my great asset.
+              I would be excited to work in your team </p>
         </div>
     </div>
   )
